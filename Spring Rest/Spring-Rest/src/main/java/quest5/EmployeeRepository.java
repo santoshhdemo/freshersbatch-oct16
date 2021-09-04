4714ae612dd2d8044f50396f6d8eb4ce067cff56
santoshhdemo <88098492+santoshhdemo@users.noreply.github.com>
2021-09-04 15:39:16 +0530

    updated

---
+++ Spring Rest/Spring-Rest/src/main/java/quest5/EmployeeRepository.java
@@ -0,0 +1,7 @@
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
+
+}
---
+++ Spring Rest/Spring-Rest/src/main/java/quest9/CustomerRepository.java
@@ -0,0 +1,9 @@
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import capg.spring.rest.quest7.Customer;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Integer> {
+
+}
